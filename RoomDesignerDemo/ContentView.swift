c731cbaaa52a57b28ac301bbf82cf847bf8aa6f5
464501551@qq.com <464501551@qq.com>
2025-10-27 16:13:13 +0800

    init

---
+++ RoomDesignerDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RoomDesignerDemo
+//
+//  Created by 格林 on 2025/10/24.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Text("Room Designer Demo")
+                .font(.title)
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ RoomDesignerDemo/RoomDesignerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomDesignerDemoApp.swift
+//  RoomDesignerDemo
+//
+//  Created by 格林 on 2025/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RoomDesignerDemoApp: App {    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
