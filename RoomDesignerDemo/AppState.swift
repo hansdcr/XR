c44a875d581e8972f2bb24a948f15c1ee9f2a8df
464501551@qq.com <464501551@qq.com>
2025-10-30 10:04:00 +0800

    迭代7+8:添加沉浸式空间文件

--- RoomDesignerDemo/AppState.swift
+++ RoomDesignerDemo/AppState.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Observation
+import RealityKit
 
 @Observable
 @MainActor
@@ -14,7 +15,16 @@ class AppState {
     // 是否在沉浸式空间中
     var isImmersive = false
 
+    // 3D 内容的根实体
+    let contentRoot = Entity()
+
     init() {
         print("AppState initialized")
+        setupContentEntity()
+    }
+
+    func setupContentEntity() {
+        contentRoot.name = "ContentRoot"
+        print("Content entity setup complete")
     }
 }
